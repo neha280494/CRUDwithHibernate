5bb1272c25dfb74e1775e3f34073f8cbe68eb0a6
neha280494 <56054764+neha280494@users.noreply.github.com>
2024-04-02 22:21:01 -0700

    Delete hibernateCRUDexample directory

--- hibernateCRUDexample/target/generated-sources/java/net/javaproj/hibernate/model/QFoo.java
+++
@@ -1,6 +0,0 @@
-package net.javaproj.hibernate.model;
-
-import static com.mysema.query.types.PathMetadataFactory.*;
-
-import com.mysema.query.types.path.*;
-
--- hibernateCRUDexample/target/generated-sources/java/net/javaproj/hibernateCRUDexample/QBar.java
+++
@@ -1,6 +0,0 @@
-package net.javaproj.hibernateCRUDexample;
-
-import static com.mysema.query.types.PathMetadataFactory.*;
-
-import com.mysema.query.types.path.*;
-
